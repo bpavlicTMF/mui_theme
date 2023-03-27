d21290ae918a9aedf896bd81c122f72a434ccd7f
Borna Pavlić <bpavlic@tmforum.org>
2023-03-27 15:15:26 +0200

    init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { theme } from '../themes/theme';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
---
+++ src/themes/theme.js
@@ -0,0 +1,59 @@
+import { createTheme } from '@mui/material/styles';
+export const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1E3F66',
+    },
+    inform: {
+      pink: '#B71A5D',
+      green: '#AABE3B',
+      purple: '#7719DF',
+      secondary: {
+        blue: '#1D52A2',
+        green: '#7E9A29',
+      },
+    },
+  },
+
+  typography: {
+    h1: {
+      fontSize: '42px',
+      lineHeight: '125%',
+    },
+    h2: {
+      fontSize: '32px',
+      letterSpacing: '0',
+      lineHeight: '125%',
+    },
+    h3: {
+      fontSize: '24px',
+      letterSpacing: '0',
+    },
+    h4: {
+      fontSize: '20px',
+      letterSpacing: 0.25,
+    },
+    h5: {
+      fontSize: '18px',
+      lineHeight: '16px',
+    },
+  },
+
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: '12.8451px 0px',
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          borderRadius: '12.8451px 0px',
+        },
+      },
+    },
+  },
+});
